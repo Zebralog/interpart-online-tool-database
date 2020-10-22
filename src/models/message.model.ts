42b4be13864d06bde7f2d6c2b8d2e8414fdc7b94
Giuseppe Roccazzella <roccazzella@zebralog.de>
2020-10-22 11:38:17 +0200

    message: some field description added to the model;

--- src/models/message.model.ts
+++ src/models/message.model.ts
@@ -11,6 +11,8 @@ export class Message extends Entity {
 
   @property({
     type: 'string',
+    name: 'bell_id',
+    description: "The source bell UUID.",
     required: true,
   })
   bell_id: string;
@@ -26,28 +28,38 @@ export class Message extends Entity {
 
   @property({
     type: 'string',
+    name: 'language',
+    description: "The main message language code.",
     required: true,
   })
   language: string;
 
   @property({
     type: 'object',
+    name: 'translations',
+    description: "Key/value object with message translations (keys are language codes).",
   })
   translations?: object;
 
   @property({
     type: 'array',
-    itemType: 'string'
+    itemType: 'string',
+    name: 'tags',
+    description: "List of tags associated to the message.",
   })
   tags?: string[];
 
   @property({
     type: 'object',
+    name: 'location',
+    description: "[lat,lng] object with the bell's location (at the time the message was sent).",
   })
   location?: object;
 
   @property({
     type: 'date',
+    name: 'timestamp',
+    description: "Date/time string (eg, '2020-10-22T07:37:45.517Z'). If null will default on message's creation timestamp.",
     'default': "$now"
   })
   timestamp?: string;
