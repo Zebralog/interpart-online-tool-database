657bb7960f9fc8666e375ac021fb5bae49b25ec1
giuseppe roccazzella <roccazzella@zebralog.de>
2020-11-11 07:52:11 +0100

    schema: updated all sql starters with @live data;

--- schema/message-and-auth-starter-just-schema.sql
+++ schema/message-and-auth-starter-just-schema.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.17  Distrib 10.3.22-MariaDB, for Linux (x86_64)
+-- MariaDB dump 10.17  Distrib 10.4.15-MariaDB, for debian-linux-gnu (x86_64)
 --
--- Host: localhost    Database: mean
+-- Host: localhost    Database: interpart
 -- ------------------------------------------------------
--- Server version	10.3.22-MariaDB
+-- Server version	10.4.15-MariaDB-1:10.4.15+maria~buster-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES UTF8 */;
+/*!40101 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -33,7 +33,7 @@ CREATE TABLE `Message` (
   `language` varchar(512) COLLATE utf8mb4_unicode_ci NOT NULL,
   `question` varchar(512) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=74 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -72,6 +72,7 @@ CREATE TABLE `UserCredentials` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
@@ -80,4 +81,4 @@ CREATE TABLE `UserCredentials` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-11-11  6:43:37
+-- Dump completed on 2020-11-11  7:49:58
--- schema/message-and-auth-starter.sql
+++ schema/message-and-auth-starter.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.17  Distrib 10.3.22-MariaDB, for Linux (x86_64)
+-- MariaDB dump 10.17  Distrib 10.4.15-MariaDB, for debian-linux-gnu (x86_64)
 --
--- Host: localhost    Database: mean
+-- Host: localhost    Database: interpart
 -- ------------------------------------------------------
--- Server version	10.3.22-MariaDB
+-- Server version	10.4.15-MariaDB-1:10.4.15+maria~buster-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES UTF8 */;
+/*!40101 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -33,7 +33,7 @@ CREATE TABLE `Message` (
   `language` varchar(512) COLLATE utf8mb4_unicode_ci NOT NULL,
   `question` varchar(512) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=74 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -101,4 +101,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-11-11  6:41:35
+-- Dump completed on 2020-11-11  7:49:58
